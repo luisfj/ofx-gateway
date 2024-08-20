b75de459671f6a22bacb072e75556bec5a374e3c
Luis Johann <luisfj_pr@hotmail.com>
2024-08-20 15:36:34 -0300

    Update cors configuration, allow preflight request

--- src/main/java/dev/luisjohann/ofxgateway/config/SecurityConfig.java
+++ src/main/java/dev/luisjohann/ofxgateway/config/SecurityConfig.java
@@ -2,27 +2,60 @@ package dev.luisjohann.ofxgateway.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.cors.reactive.CorsUtils;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebFilter;
+import org.springframework.web.server.WebFilterChain;
+import reactor.core.publisher.Mono;
 
 @Configuration
 @EnableWebFluxSecurity
 public class SecurityConfig {
 
-        @Bean
-        public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity serverHttpSecurity) {
-                return serverHttpSecurity
-                                .csrf(ServerHttpSecurity.CsrfSpec::disable)
-                                .authorizeExchange(exchange -> exchange
-                                                .pathMatchers("/eureka/**",
-                                                                "/api/*/permission-checker/public/**",
-                                                                "/admin/**")
-                                                .permitAll()
-                                                .anyExchange().authenticated())
-                                .oauth2ResourceServer((oauth) -> oauth
-                                                .jwt(Customizer.withDefaults()))
-                                .build();
-        }
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity serverHttpSecurity) {
+        return serverHttpSecurity
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange(exchange -> exchange
+                        .pathMatchers("/eureka/**",
+                                "/api/*/permission-checker/public/**",
+                                "/admin/**")
+                        .permitAll()
+                        .anyExchange().authenticated())
+                .oauth2ResourceServer((oauth) -> oauth
+                        .jwt(Customizer.withDefaults()))
+                .build();
+    }
+
+    @Bean
+    public WebFilter corsFilter() {
+        return (ServerWebExchange ctx, WebFilterChain chain) -> {
+            ServerHttpRequest request = ctx.getRequest();
+            if (CorsUtils.isCorsRequest(request)) {
+                ServerHttpResponse response = ctx.getResponse();
+                HttpHeaders headers = response.getHeaders();
+                headers.add("Access-Control-Allow-Origin", "http://localhost:3000");
+                headers.add("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS, PATCH");
+                headers.add("Access-Control-Max-Age", "7200");
+                headers.add("Access-Control-Allow-Headers",
+                        "x-requested-with, authorization, Content-Type, Content-Length, Authorization, credential, X-XSRF-TOKEN");
+                if (request.getMethod() == HttpMethod.OPTIONS) {
+                    response.setStatusCode(HttpStatus.OK);
+                    return Mono.empty();
+                }
+            }
+            return chain.filter(ctx);
+        };
+    }
+
+
 }
